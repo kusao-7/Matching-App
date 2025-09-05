f4949a33d6bad987eb9f66fe335cfd75e4dee64f
koosh <kooshiroo0426@gmail.com>
2025-09-05 16:04:42 +0900

    青山commit

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("青山です");
         System.out.println("ナイス！");
+        System.out.println("kooshブランチ");
     }
 }
